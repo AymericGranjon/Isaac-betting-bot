7daf25a828ccb2b31a4ae90d92266533d2e98fb8
PassionDrama <aymeric.granjon@gmail.com>
2018-10-27 10:27:32 +0200

    oops

--- commandBetter.py
+++ commandBetter.py
@@ -111,7 +111,7 @@ and t1.race_id = t2.race_id;""").format(racer1_name, racer2_name)
         if  int(coin) <= 0 :
             await self.bot.send_message(bot_channel,"Stop trying to break me {}".format(better.coin, emoji))
             return
-        winner = self.session.query(Racer).filter(Racer.name.lower() == winner_name.lower()).first()
+        winner = self.session.query(Racer).filter(Racer.name == winner_name).first()
         if race.racer1_id == winner.id :
             odd = race.odd1
         elif race.racer2_id == winner.id :
